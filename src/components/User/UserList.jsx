d0734ff786c38e8bb03db6c0ebd0b1f4fc7a365a
Sigifredo Chacón Balladares <24.sigi@estudiantec.cr>
2024-09-21 00:38:37 -0600

    se agrego el buscador en usuarios

--- src/components/User/UserList.jsx
+++ src/components/User/UserList.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import PropTypes from 'prop-types'; // Importa PropTypes
-import { getUsers, deleteUser } from '../../services/userService'; // Servicio para obtener usuarios y eliminar
+import PropTypes from 'prop-types';
+import { getUsers, deleteUser, getUserById } from '../../services/userService'; // Importa getUserById
 import {
     Card,
     Table,
@@ -10,14 +10,14 @@ import {
     TableHeaderCell,
     TableRow,
     Title,
-    Badge
+    Badge,
+    TextInput
 } from '@tremor/react';
-
-
-
 
 function UserList({ onEdit }) {
     const [users, setUsers] = useState([]); // Estado para almacenar la lista de usuarios
+    const [searchTerm, setSearchTerm] = useState(''); // Estado para almacenar el término de búsqueda
+    const [searchResult, setSearchResult] = useState(null); // Estado para almacenar el resultado de la búsqueda
 
     useEffect(() => {
         fetchUsers(); // Llama a la función para obtener usuarios al montar el componente
@@ -35,6 +35,7 @@ function UserList({ onEdit }) {
 
     // Función para manejar la eliminación de un usuario
     const handleDelete = async (id) => {
+
         try {
             await deleteUser(id); // Llama al servicio para eliminar un usuario por ID
             setUsers(users.filter((user) => user.CedulaCarnet !== id)); // Actualiza la lista de usuarios eliminando el usuario borrado
@@ -43,9 +44,29 @@ function UserList({ onEdit }) {
         }
     };
 
+    // Función para manejar el cambio en el input de búsqueda
+    const handleSearchChange = async (e) => {
+        const value = e.target.value;
+        setSearchTerm(value); // Actualiza el estado con el valor del input de búsqueda
+
+        if (value !== '') {
+            try {
+                const user = await getUserById(Number(value)); // Llama a la API para buscar el usuario por cédula
+                setSearchResult(user ? [user] : []); // Si se encuentra el usuario, lo muestra; si no, muestra un array vacío
+            } catch (error) {
+                console.error('Error al buscar usuario:', error);
+                setSearchResult([]); // Si ocurre un error, limpia el resultado de la búsqueda
+            }
+        } else {
+            setSearchResult(null); // Si no hay término de búsqueda, limpia el resultado de la búsqueda
+        }
+    };
+
+    // Determina qué lista de usuarios mostrar: la búsqueda o todos los usuarios
+    const displayedUsers = searchResult !== null ? searchResult : users;
 
     return (
-        <Card style={{ border: '0.5px solid #00000085', borderRadius: '12px', padding: '16px'}}>
+        <Card style={{border: '0.5px solid #00000085', borderRadius: '12px', padding: '16px'}}>
             <Title>
                 Usuarios
                 <Badge style={{
@@ -56,44 +77,56 @@ function UserList({ onEdit }) {
                     padding: '3px 7px',           // Relleno más pronunciado
                     fontWeight: 'bold',           // Texto en negrita
                     fontSize: '1rem',          // Tamaño de fuente más pequeño
-                }}>{users.length}</Badge>
+                }}>{displayedUsers.length}</Badge>
             </Title>
+            <div className="w-full flex justify-center">
+                <div className="w-96">
+                    <TextInput
+                        placeholder="Buscar por Cédula/Carnet"
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                        className="search-input w-full text-center rounded-full px-4 py-3 border-2 border-gray-300 outline-none transition-colors duration-300 ease-in-out focus:border-green-500 bg-transparent"
+                    />
+                </div>
+            </div>
+
+
             <Table className="mt-8">
                 <TableHead>
-                    <TableRow >
-                        <TableHeaderCell >
+                    <TableRow>
+                        <TableHeaderCell>
                             Cédula/Carnet
                         </TableHeaderCell>
-                        <TableHeaderCell >
+                        <TableHeaderCell>
                             Nombre
                         </TableHeaderCell>
-                        <TableHeaderCell >
+                        <TableHeaderCell>
                             Correo Email
                         </TableHeaderCell>
-                        <TableHeaderCell >
+                        <TableHeaderCell>
                             Correo Institucional
                         </TableHeaderCell>
-                        <TableHeaderCell >
+                        <TableHeaderCell>
                             Teléfono
                         </TableHeaderCell>
-                        <TableHeaderCell >
+                        <TableHeaderCell>
                             Teléfono2
                         </TableHeaderCell>
                         <TableHeaderCell>
                             Dirección
                         </TableHeaderCell>
-                        <TableHeaderCell >
+                        <TableHeaderCell>
                             Rol
                         </TableHeaderCell>
-                        <TableHeaderCell >
+                        <TableHeaderCell>
                             Acciones
                         </TableHeaderCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {users.map((user) => (
+                    {displayedUsers.map((user) => (
                         <TableRow key={user.CedulaCarnet}>
-                            <TableCell >
+                            <TableCell>
                                 {user.CedulaCarnet}
                             </TableCell>
                             <TableCell>{user.Nombre}</TableCell>
@@ -104,17 +137,19 @@ function UserList({ onEdit }) {
                             <TableCell>{user.Direccion}</TableCell>
                             <TableCell>{user.NombreRol}</TableCell>
                             <TableCell className="text-left">
-                                <button onClick={() => onEdit(user)} >
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                <button onClick={() => onEdit(user)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"/>
                                     </svg>
-
                                 </button>
                                 <button onClick={() => handleDelete(user.CedulaCarnet)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"/>
                                     </svg>
-
                                 </button>
                             </TableCell>
                         </TableRow>
@@ -127,7 +162,7 @@ function UserList({ onEdit }) {
 
 // Validación de PropTypes para el componente
 UserList.propTypes = {
-    onEdit: PropTypes.func.isRequired, // `onEdit` es una función requerida
+    onEdit: PropTypes.func.isRequired,
 };
 
 export default UserList;
